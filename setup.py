7e14a0d6db10c1985e370fb90c31be3f9b732caf
Thibault Cohen <titilambert@gmail.com>
2017-07-31 01:07:01 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,24 @@ session = PipSession()
 install_reqs = parse_requirements('requirements.txt', session=session)
 test_reqs = parse_requirements('test_requirements.txt', session=session)
 
+packages = ['pynuance',
+            'pynuance.libs',
+            ]
+            
+
 setup(name='pynuance',
       version='0.1.0',
       description='Python wrapper for Nuance Communications services',
       author='Thibault Cohen',
       author_email='titilambert@gmail.com',
       url='http://github.org/titilambert/pynuance',
-      packages=['pynuance'],
+      packages=packages,
       entry_points={
           'console_scripts': [
               'pynuance = pynuance.__main__:main'
           ]
       },
+      package_dir={'pynuance': 'pynuance'},
       install_requires=[str(r.req) for r in install_reqs],
       tests_require=[str(r.req) for r in test_reqs],
 )
